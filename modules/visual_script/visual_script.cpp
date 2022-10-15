29d62e1b15b74b0ccc50b60c0bdec6058300d9fe
Andreas Van Vooren <andreas.van.vooren@gmail.com>
2022-10-15 16:38:34 +0200

    Refactor if-statement that would eval to no-op and cause an identical branch warning

--- modules/visual_script/visual_script.cpp
+++ modules/visual_script/visual_script.cpp
@@ -33,6 +33,7 @@
 #include "core/core_string_names.h"
 #include "core/os/os.h"
 #include "core/project_settings.h"
+#include "core/ustring.h"
 #include "scene/main/node.h"
 #include "visual_script_func_nodes.h"
 #include "visual_script_nodes.h"
@@ -1696,11 +1697,7 @@ Variant VisualScriptInstance::_call_internal(const StringName &p_method, void *p
 			}
 
 			next = node->sequence_outputs[output];
-			if (next) {
-				VSDEBUG("GOT NEXT NODE - " + itos(next->get_id()));
-			} else {
-				VSDEBUG("GOT NEXT NODE - NULL");
-			}
+			VSDEBUG(String("GOT NEXT NODE - ") + (next ? itos(next->get_id()) : String("NULL")));
 		}
 
 		if (flow_stack) {
